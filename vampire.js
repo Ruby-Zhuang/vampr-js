c5405f06cd09c126eda5180982a40aca73ab1968
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-16 17:15:14 -0400

    Add functions to vampire tree

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no creator is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal);
   }
 
   /** Stretch **/
@@ -36,9 +46,44 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampire1 = this;
+    let currentVampire2 = vampire;
 
+    while (currentVampire1) {
+      while (currentVampire2) {
+        if (currentVampire1 === currentVampire2) {
+          return currentVampire1;
+        }
+        currentVampire2 = currentVampire2.creator;
+      }
+      currentVampire2 = vampire;
+      currentVampire1 = currentVampire1.creator;
+    }
+
+    return;
   }
 }
 
-module.exports = Vampire;
+// Create new nodes/vampires
+const original = new Vampire("Original", 2000);
+const ansel    = new Vampire("ansel", 2010);
+const bart     = new Vampire("bart", 2010);
+const elgort   = new Vampire("elgort", 2015);
+const sarah    = new Vampire("sarah", 2015);
+const andrew   = new Vampire("andrew", 2020);
 
+// Link offsptring vampires to their creator
+original.addOffspring(ansel);
+original.addOffspring(bart);
+
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+
+elgort.addOffspring(andrew);
+
+// Tests
+// console.log("------------------------------------------------------");
+// console.log("Ansel's creator: ", ansel.creator);
+// console.log("Number of offspring: ", ansel.numberOfOffspring);
+
+module.exports = Vampire;
